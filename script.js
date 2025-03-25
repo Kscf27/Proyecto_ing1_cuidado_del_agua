45a6c5521f3617aa6f5295d0a320c7213a2fe01d
Kscf27 <freedom092j@gmail.com>
2025-03-25 14:17:37 -0500

    Primer commit
    Subida de archivos locales a git

---
+++ script.js
@@ -0,0 +1,8 @@
+/*document.addEventListener("DOMContentLoaded", function () {
+    alert("¡Bienvenido! Aprende a ahorrar agua jugando y explorando.");
+});*/
+
+function toggleMenu() {
+    let menu = document.querySelector(".menu-list");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
